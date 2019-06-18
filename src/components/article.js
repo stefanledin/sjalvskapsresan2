6904b9d994686c8f47e35a865fe2757a61e26eb2
Stefan Ledin <stefan.ledin@gmail.com>
2019-06-18 15:53:54 +0200

    Fortsätter testa

--- src/components/article.js
+++ src/components/article.js
@@ -3,23 +3,40 @@ import { graphql } from 'gatsby';
 import neuschwanstein from '../images/neuschwanstein.jpg'
 
 export default ({data}) => {
+    if (!data) {
+        throw new Error('Nåt fel!');
+    }
     const post = data.wordpressPost;
+    console.log(post);
     return (
         <article className="bg-white mb-6 shadow-lg w-9/12 mx-auto">
-            <img src={neuschwanstein} alt="Neuschwanstein" />
             <div className="p-6">
                 <span className="pacifico text-2xl text-orange-500 text-center block">Dag 6: München</span>
                 <h1 className="mb-6 text-5xl pacifico text-red-600 text-center">{post.title}</h1>
-                <div className="pt-4 pb-4 mx-auto w-9/12" dangerouslySetInnerHTML={{__html: post.content}} />
+                <div className="pt-4 pb-4 mx-auto w-9/12" />
             </div>
+            <img src={neuschwanstein} alt="Neuschwanstein" />
         </article>
     )
 }
+
+/*
+export const query = graphql`
+    query {
+        site {
+            siteMetadata {
+            title
+            }
+        }
+    }
+`
+*/
 
 export const query = graphql`
     query($slug: String) {
         wordpressPost(slug: {eq: $slug}) {
             id
+            slug
             title
             content
         }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import Article from '../components/article';
 const IndexPage = ( { data } ) => {
   return (
     <div className="relative">
-      <aside className="m-4 w-3/12 inset-y-0 z-1 absolute">
+      <aside className="m-4 w-3/12 inset-y-0 z-1 absolute hidden">
         <div className="bg-orange-200 shadow rounded-lg">
-          <h2 className="m-2 pacifico text-3xl text-red-700"><span role="img">🇸🇪</span> Karlskrona</h2>
+          <h2 className="m-2 pacifico text-3xl text-red-700"><span role="img" aria-label="Sverige" aria-labelledby="Sverige">🇸🇪</span> Karlskrona</h2>
         </div>
       </aside>
       <main className="container mx-auto">
         {data.allWordpressPost.edges.map(({node}) => (
-          <h2 className="m-2 pacifico text-3xl text-red-700" key={node.id}><a href={`/${node.slug}`}>{node.title}</a></h2>
+          <h2 className="m-2 pacifico text-3xl text-red-700" key={node.id}><a href={`/${node.id}`}>{node.title}</a></h2>
         ))}
         <header className="text-center">
           <h1 className="mb-6 text-4xl pacifico text-red-700">Självskapsresan 2(019)</h1>
