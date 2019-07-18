e5dd91d1f14d9c14c4860b3d44fc9ffe7206bfdd
Stefan Ledin <stefan.ledin@gmail.com>
2019-07-18 11:31:26 +0200

    responsivt...

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -50,16 +50,16 @@ export default class Sidebar extends React.Component {
                         to={`/${post.slug}`} 
                         activeClassName="border-orange-700 active"
                         onClick={this.props.onNavigation}
-                        className="p-4 flex flex-row-reverse w-full border-l-4 border-transparent hover:border-red-600">
-                            <div className="w-2/4">
+                        className="p-4 flex flex-col md:flex-row-reverse w-full border-l-4 border-transparent hover:border-red-600">
+                            <div className="w-full md:w-2/4">
                                 {post.featured_media &&
                                     <Img fluid={post.featured_media.localFile.childImageSharp.fluid} />
                                 }
                             </div>
-                            <div className="w-2/4">
-                                <div className="px-4">
-                                    <span className="block text-sm">{post.date}</span>
-                                    <span className="pacifico text-3xl">{post.title}</span>
+                            <div className="w-full md:w-2/4">
+                                <div className="pt-2 md:pt-0 md:px-4">
+                                    <span className="block text-xs md:text-sm">{post.date}</span>
+                                    <span className="pacifico text-xl md:text-3xl">{post.title}</span>
                                 </div>
                             </div>
                     </Link>
@@ -74,7 +74,7 @@ export default class Sidebar extends React.Component {
                 <li key={index} className={'my-6 border-orange-200 border hover:border-orange-300 hover:border-t-2 ' + currentLiClasses}>
                     <span 
                         onClick={(e) => this.setCurrentCategory(index, e)} 
-                        className={currentSpanClasses + ' p-2 pl-4 cursor-pointer block pacifico text-3xl text-red-700'}>
+                        className={currentSpanClasses + ' p-2 xxx-pl-4 cursor-pointer block pacifico text-2xl md:text-3xl text-red-700'}>
                         {group.category.name}
                     </span>
                     {this.state.currentCategories.indexOf(index) !== -1 &&
